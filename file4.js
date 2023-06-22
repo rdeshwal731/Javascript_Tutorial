b0d3ae836fc9919b8b9dea73a067e60740565eb0
Raghav Deshwal <84080231+rdeshwal731@users.noreply.github.com>
2023-06-22 10:44:14 +0530

    Add files via upload

---
+++ file4.js
@@ -0,0 +1,98 @@
+/* 
+// Metacrafters - Beginning Javascript
+//
+// This is a javascript playground for testing your javascript code!
+// When you are ready to test, simply right click and select "Run Code"
+// to see the result print below. If you have more then one snippet of code,
+// you can highlight the code you want to test, and then right click and select "Run Code"
+*/
+
+//Introduction to arrays
+
+/*
+store multiple values in one using js arrays
+nest one arrat within another array
+access array data with indexes
+modify array with indexes
+access multi dimensional arrays with indexes
+manipulate ayyars with push/pop/shift/unshift method
+build java objects
+*/
+
+var ourName = [[["yo"], ["yo"]], ["honey"], ["singh"]];
+let myName = [["honey"],["singh"]];
+const yoName = [["yo"],["yo"]];
+const yourName = ourName[0][0]; 
+const hisName = ['brown rang',"blue eyes",'desi kalakar','love dose','stardom','thousand miles'];
+
+
+console.log(ourName[0][0]);
+//console.log(ourName[0]);
+console.log(yourName);
+hisName.push("dope shope");
+//console.log(hisName);
+hisName.pop();
+//console.log(hisName);
+hisName.shift();
+//console.log(hisName);
+hisName.unshift('brown rang','dope shope','siftaan','satan','bring me back');
+console.log(hisName);
+
+
+
+/*
+const num1 = 1+2+3+4+5;
+const num2 = 7+8+9+10;
+
+console.log (num1);
+console.log (num2);
+
+const dif = num2 - num1;
+const sum = num1 + num2;
+const mul = num1 * num2;
+const div = num2 / num1;
+
+
+console.log (dif);
+console.log (sum);
+console.log (mul);
+console.log (div);
+console.log (div*div);
+console.log (div/div);
+console.log (num2%num1);
+
+for( var i=1; i<=5; i++) 
+{
+ console.log(i);
+}
+console.log (ourName);
+console.log (yourName);
+console.log (myName);
+
+const convo = 'I exclaims to aryan, "yoyo honeysingh is the GOAT!!"';
+console.log (convo);
+
+const goodStr = 'Aryan asks me, "Hey, let\'s listen yoyo!"'; 
+const badStr = 'I said, "Let\'s go!"';
+const ourStr = "I came first. " + "You came second.";
+const ourStr2 = "Hello, it's your boy " + ourName + ", let's go, pepole";
+const anAdj = " awesome!";
+let newStr = "honey singh is";
+newStr += anAdj;
+
+console.log (goodStr);
+console.log (badStr);
+console.log (ourStr);
+console.log (ourStr2);
+console.log (newStr);
+console.log (goodStr.length);
+*/
+
+
+
+
+
+
+
+
+

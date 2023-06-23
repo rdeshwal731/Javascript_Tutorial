38871121fffdc6164aafa847f475e984a1e088e0
Raghav Deshwal <84080231+rdeshwal731@users.noreply.github.com>
2023-06-23 22:42:49 +0530

    Add files via upload

---
+++ file7.js
@@ -0,0 +1,68 @@
+//js loops and functions
+
+//js loops
+/*
+Iterate with JS While Loops
+Iterate with JS For Loops
+Iterate Odd Numbers With a For Loop
+Count Backwards With a For Loop
+Iterate Through an Array with a For Loop
+Nesting For Loops
+Iterate with JavaScript Do...While Loops
+Use the parseInt Function
+*/
+/*
+const ourArray = [];
+const arr = [10,9,8,7,6,5,4,3,2,1];
+const arr2 =[[1,2],[3,4],[5,6]];
+let i = 0;
+/*
+while(i<5)
+{
+    ourArray.push(i);
+    i++;
+    console.log(ourArray);
+}
+*
+for (let i = 0; i < 5; i++) 
+{
+  ourArray.push(i);
+  console.log(ourArray);
+}
+*
+for (let i = 0; i < 10; i += 2) 
+{
+  ourArray.push(i);
+  console.log(ourArray);
+}
+*
+for (let i = 10; i>0; i-=2)
+{
+    ourArray.push(i);
+    console.log(ourArray);
+}
+*
+for (let i = 0; i< arr.length; i++)
+{
+    console.log(arr[i]);
+}
+*
+for (let i =0; i< arr2.length; i++)
+{
+    for (let j =0; j<arr2.length; j++)
+    {
+        console.log(arr2[i][j]);
+    }
+}
+*
+do
+{
+    ourArray.push(i);
+    i++;
+    console.log(ourArray);
+}
+while (i<5);
+*
+const a = parseInt("007");
+console.log(a);
+*/
